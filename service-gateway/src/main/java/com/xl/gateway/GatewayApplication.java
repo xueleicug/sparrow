305145c1a9ffa1379b60611344295a2b4072cac6
xuelei <xueleicug@163.com>
2017-11-23 23:45:28 +0800

    add a gateway service

---
+++ service-gateway/src/main/java/com/xl/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.xl.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringCloudApplication
+@Slf4j
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplicationBuilder app = new SpringApplicationBuilder(GatewayApplication.class);
+        app.web(true);
+        app.bannerMode(Banner.Mode.CONSOLE);
+        app.run();
+        log.info("^_^ ####################################### ^_^");
+        log.info("^_^ ### Gateway Application was started ### ^_^");
+        log.info("^_^ ####################################### ^_^");
+    }
+
+}
