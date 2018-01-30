2e991cdb9be3f24f8443699af3004189e501bd94
xuelei <xueleicug@163.com>
2018-01-30 21:58:28 +0800

    do some test about RestTemplate

--- service-base/src/main/java/com/xl/base/controller/HelloController.java
+++ service-base/src/main/java/com/xl/base/controller/HelloController.java
@@ -4,10 +4,8 @@ import com.xl.base.entity.Book;
 import com.xl.base.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Random;
 
@@ -34,20 +32,25 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/book/all", method = {RequestMethod.POST, RequestMethod.GET})
-    @ResponseBody
     public List<Book> getAllBook() {
         return bookService.getAllBooks();
     }
 
     @RequestMapping(value = "/book/press", method = {RequestMethod.POST, RequestMethod.GET})
-    @ResponseBody
     public List<String> getAllPress() {
         return bookService.getAllPress();
     }
 
     @RequestMapping(value = "/book/all/desc", method = {RequestMethod.POST, RequestMethod.GET})
-    @ResponseBody
     public List<Book> getAlldesc() {
         return bookService.getAllBookOrderByBuytimeDesc();
+    }
+
+    @RequestMapping(value = "/test/{path}", method = {RequestMethod.POST, RequestMethod.GET})
+    public String testParameter(@PathVariable String path,
+                                @RequestParam String userName,
+                                @RequestParam String userType) {
+
+        return path + "  " + userName + "  " + userType;
     }
 }
---
+++ service-base/src/test/java/com/xl/base/controller/HelloControllerTest.java
@@ -0,0 +1,53 @@
+package com.xl.base.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.net.URL;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+//@AutoConfigureMockMvc together with @SpringBootTest to inject a MockMvc instance
+@Slf4j
+public class HelloControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/test/cug");
+    }
+
+    @Test
+    public void getAllBook() {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("userName", "xuelei");
+        params.add("userType", "master");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Connection", "keep-alive");
+        HttpEntity<MultiValueMap> httpEntity = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(base.toString(), httpEntity, String.class);
+        log.info(response.getBody());
+    }
+}
